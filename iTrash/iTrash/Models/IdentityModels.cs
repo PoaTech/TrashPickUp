c925e4f1035a9ea3e675722f8ac268a29cbfa00d
Msatkin <mscottaa@gmail.com>
2016-11-21 17:15:46 -0600

    initial migration setup

--- iTrash/iTrash/Models/IdentityModels.cs
+++ iTrash/iTrash/Models/IdentityModels.cs
@@ -3,6 +3,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace iTrash.Models
 {
@@ -29,5 +30,7 @@ namespace iTrash.Models
         {
             return new ApplicationDbContext();
         }
+
+        public DbSet<Truck> Truck { get; set; }
     }
 }
