fd986bf30ae09f13b1083ef2c60df24004ea15f8
Msatkin <mscottaa@gmail.com>
2016-11-30 14:26:26 -0600

    Routes almost done, just need to add Google maps API

--- iTrash/iTrash/Controllers/ManageController.cs
+++ iTrash/iTrash/Controllers/ManageController.cs
@@ -405,6 +405,16 @@ namespace iTrash.Controllers
             model.GetData(userID, db);
             return View(model);
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Route(RouteViewModel model)
+        {
+            string userID = User.Identity.GetUserId();
+            var newModel = new RouteViewModel();
+            newModel.GetData(userID, db);
+            newModel.route = model.truckId;
+            return View(newModel);
+        }
         #region Helpers
         // Used for XSRF protection when adding external logins
         private const string XsrfKey = "XsrfId";
